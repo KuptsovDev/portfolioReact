02eb2375a2a0907bc94891d2760a04b4a44b20e0
KuptsovAleksandr <kuptsov.dev@gmail.com>
2025-03-10 19:52:26 +0500

    created sections Projects

---
+++ src/common/ProjectCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ProjectCard() {
+  return (
+    <a href="/" target="_blank">
+      <img className="hover" src={viberr} alt="Viberr logo" />
+      <h3>Viberr</h3>
+      <p>Streaming App</p>
+    </a>
+  );
+}
+
+export default ProjectCard;
